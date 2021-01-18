04d5603fbe0bcfd9a91c1c95a461d27ba08d5771
Joel Ermantraut <joelermantraut@gmail.com>
2021-01-17 21:52:42 -0300

    Added function to find chat

---
+++ automation_scripts/mouse_control.py
@@ -0,0 +1,173 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+Class to simplify the use of the mouse.
+
+TODO:
+
+    - Añadir las funciones de monitoreo.
+
+"""
+
+from pynput import mouse
+from screeninfo import get_monitors
+
+class MouseControl(object):
+    """Class to simplify the use of the mouse"""
+
+    def __init__(self):
+        super(MouseControl, self).__init__()
+
+        self.mouse_buttons = {
+            'left': mouse.Button.left,
+            'middle': mouse.Button.middle,
+            'right': mouse.Button.right
+        }
+        self.transpose_button = {key: value for (value, key) in self.mouse_buttons.items()}
+        self.on_move_f = None
+        self.on_click_f = None
+        self.on_scroll_f = None
+        self.init()
+
+    def init(self):
+        """
+        Runs some init functions.
+
+         - Inits controller.
+        """
+        self.controller = mouse.Controller()
+
+    def mouse_move(self, move=None, rel=False, drag=False, function=None, iters=1):
+        """"
+        Moves the mouse taking care of:
+
+            - If receives a function, executes it before each cycle.
+            - If rel, enables relative move.
+            - If drag, moves the mouse with the left button pressed.
+            - If move == None, return the current position of the mouse.
+        """
+
+        if move == None:
+            return self.controller.position()
+
+        if drag:
+            self.controller.press(mouse.Button.left)
+            if rel:
+                self.controller.move(*move)
+            else:
+                self.controller.position = move
+
+            self.controller.release(self.mouse_buttons['left'])
+
+        if function:
+            for i in range(iters):
+                x, y = function()
+                self.controller.position = (x, y)
+        else:
+            if rel:
+                self.controller.move(*move)
+            else:
+                self.controller.position = move
+
+    def click(self, button, clicks=1):
+        """
+        Click in the current position of the mouse. 
+        """
+
+        button = self.mouse_buttons[button]
+
+        self.controller.click(button, clicks)
+
+    def scroll(self, amount, percent=False):
+        """
+        Scrolls up or down.
+
+         - If percent, moves that percent of the screen.
+        """
+        if percent:
+            first_monitor_height = get_monitors()[0].height
+
+            amount = int((amount / 100) * first_monitor_height)
+
+        self.controller.scroll(0, -amount)
+
+    # --------------------------------------------------
+    # --------------- MOUSE MONITORING -----------------
+    # --------------------------------------------------
+
+    def on_move(self, x, y):
+        """
+        On move event.
+        """
+
+        if self.on_move_f != None:
+            self.on_move_f(x, y)
+
+    def on_click(self, x, y, button, pressed):
+        """
+        On click event.
+        """
+        button = self.transpose_button[button]
+
+        if self.on_click_f != None:
+            self.on_click_f(x, y, button, pressed)
+
+    def on_scroll(self, x, y, dx, dy):
+        """
+        On scroll event.
+        """
+        if self.on_scroll_f != None:
+            self.on_scroll_f(dx, dy)
+
+    def start_listening(
+            self,
+            on_move=None,
+            on_click=None,
+            on_scroll=None,
+            blocking=False
+        ):
+        """
+        Function that configures the listening using
+        arguments to set all properties.
+        """
+
+        self.on_move_f = on_move 
+        self.on_click_f = on_click
+        self.on_scroll_f = on_scroll
+
+        if blocking:
+            with mouse.Listener(
+                on_move=self.on_move,
+                on_click=self.on_click,
+                on_scroll=self.on_scroll
+            ) as listener:
+                listener.join()
+        else:
+            listener = mouse.Listener(
+                on_move=self.on_move,
+                on_click=self.on_click,
+                on_scroll=self.on_scroll
+            )
+            listener.start()
+
+def on_move_p(x, y):
+    print(x, ':', y)
+
+def on_click_p(x, y, button, pressed):
+    print(x, ':', y, ':', button, ':', pressed)
+
+def on_scroll_p(dx, dy):
+    print(dx, ':', dy)
+
+def main():
+    mouse_controller = MouseControl()
+
+    mouse_controller.start_listening(
+            on_move=on_move_p,
+            on_click=on_click_p,
+            on_scroll=on_scroll_p,
+            blocking=True)
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -25,15 +25,24 @@ _2AuNk:
         DuUXI:
             _1awRl copyable-text selectable-text: Input
 
+_1yHR2:
+    Barcode
+
 """
 
 
 from automation_scripts.web_scrapping import WebScrapper
+from automation_scripts.mouse_control import MouseControl
 import time
 
 class WhatsMatic(object):
-    """Class to control WhatsApp and automate it."""
+    """App to control WhatsApp and automate it."""
     def __init__(self):
+        self.list_of_chats_element = "._3soxC"
+        self.chat_button_element = "._3Pwfx"
+        self.chat_element = "._3Pwfx ._1C6Zl ._3es8f span"
+        self.input_element = "._2AuNk ._1awRl"
+        self.barcode_element = "._1yHR2"
         self.init() 
 
     def init(self):
@@ -45,9 +54,57 @@ class WhatsMatic(object):
             "https://web.whatsapp.com"
         )
 
-        time.sleep(10)
+        self.mouse = MouseControl()
 
-        self.close()
+        self.wait_for_scan()
+
+        time.sleep(2)
+
+    def wait_for_scan(self):
+        """
+        When WhatsApp is open, the barcode is shown.
+        This functions waits after it is scanned.
+        """
+        while self.driver.get_elements(self.barcode_element):
+            time.sleep(1)
+
+    def is_element(self, attribute, value, elements):
+        """
+        Searches an element with the given properties.
+        """
+        for element in elements:
+            chat_properties = self.driver.get_all_properties(element)[0]
+
+            if value in chat_properties.values():
+                return element
+
+        return None
+
+    def search_chat(self, name):
+        """
+        Search for a chat with the name given and returns the element.
+
+        Elements cannot be accessed if they are not loaded. Though, the
+        script will scroll up to last element be the same as the last scrolled
+        last element.
+        """
+        self.mouse.mouse_move((30, 400))
+        # Moves mouse to list of chats to scroll
+
+        list_of_chats = self.driver.get_elements(self.list_of_chats_element)[0]
+        an_element = self.driver.get_elements(self.chat_button_element)[0]
+        iters = list_of_chats.size['height'] // (an_element.size['height'] * 6) 
+
+        for i in range(iters):
+            elements = self.driver.get_elements(self.chat_element)
+            element = self.is_element('title', name, elements)
+            if element:
+                return element
+
+            self.mouse.scroll(1, True) # 1% of height
+            time.sleep(1)
+
+        return None
 
     def close(self):
         """
@@ -57,6 +114,8 @@ class WhatsMatic(object):
 
 def main():
     whatsmatic = WhatsMatic()
+
+    print(whatsmatic.search_chat('Danilo Coletto'))
 
 if __name__ == "__main__":
     main()
